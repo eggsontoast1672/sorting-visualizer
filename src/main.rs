43f395dbda1327564093714648f159a883736f6d
Paul Zupan <paul.zupan.13@gmail.com>
2025-09-06 13:42:20 -0700

    Finish first prototype

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,23 @@
+mod sorters;
+
+use ::rand::seq::SliceRandom;
 use clap::{Parser, ValueEnum};
+use macroquad::prelude::*;
+
+use crate::sorters::{BubbleSorter, Sorter};
+
+const WINDOW_WIDTH: i32 = 800;
+const WINDOW_HEIGHT: i32 = 600;
+
+fn window_conf() -> Conf {
+    Conf {
+        window_title: "Sorting Visualizer".into(),
+        window_width: WINDOW_WIDTH,
+        window_height: WINDOW_HEIGHT,
+        window_resizable: false,
+        ..Default::default()
+    }
+}
 
 #[derive(Clone, Debug, ValueEnum)]
 enum SortingAlgorithm {
@@ -7,18 +26,59 @@ enum SortingAlgorithm {
     Quick,
 }
 
+impl std::fmt::Display for SortingAlgorithm {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let s = format!("{:?}", self);
+        write!(f, "{}", s.to_lowercase())
+    }
+}
+
 #[derive(Parser)]
 struct Args {
-    #[arg(long)]
+    #[arg(short, long, default_value_t = SortingAlgorithm::Quick)]
     algorithm: SortingAlgorithm,
 
-    #[arg(long)]
+    #[arg(short, long, default_value_t = 100)]
     num_elements: usize,
 }
 
-fn main() {
-    let args = Args::parse();
+fn create_data(num_elements: usize) -> Vec<usize> {
+    let mut data: Vec<_> = (1..=num_elements).collect();
+    data.shuffle(&mut ::rand::rng());
+    data
+}
 
-    println!("Algorithm: {:?}", args.algorithm);
-    println!("Elements: {}", args.num_elements);
+fn create_sorter(alg: SortingAlgorithm, data_length: usize) -> impl Sorter {
+    match alg {
+        SortingAlgorithm::Bogo => todo!(),
+        SortingAlgorithm::Bubble => BubbleSorter::new(data_length),
+        SortingAlgorithm::Quick => todo!(),
+    }
+}
+
+fn draw_data(data: &[usize]) {
+    for (index, number) in data.iter().enumerate() {
+        let block_width = WINDOW_WIDTH as f32 / data.len() as f32;
+        let block_height = *number as f32 / data.len() as f32 * WINDOW_HEIGHT as f32;
+        draw_rectangle(
+            index as f32 * block_width,
+            WINDOW_HEIGHT as f32 - block_height,
+            block_width,
+            block_height,
+            WHITE,
+        );
+    }
+}
+
+#[macroquad::main(window_conf)]
+async fn main() {
+    let args = Args::parse();
+    let mut data = create_data(args.num_elements);
+    let mut sorter = create_sorter(args.algorithm, data.len());
+
+    loop {
+        draw_data(&data);
+        sorter.step(&mut data);
+        next_frame().await;
+    }
 }
---
+++ src/sorters.rs
@@ -0,0 +1,48 @@
+pub trait Sorter {
+    fn step(&mut self, data: &mut [usize]) -> bool;
+}
+
+pub struct BubbleSorter {
+    current: usize,
+    // Points to the index of the first element of the sorted block at the end of the slice.
+    first_sorted: usize,
+}
+
+impl BubbleSorter {
+    pub const fn new(data_length: usize) -> Self {
+        Self {
+            current: 0,
+            first_sorted: data_length,
+        }
+    }
+}
+
+impl Sorter for BubbleSorter {
+    fn step(&mut self, data: &mut [usize]) -> bool {
+        // If the first index of the sorted block is the beginning of the slice, that means that
+        // the entire slice is sorted and we are done.
+        if self.first_sorted == 0 {
+            return true;
+        }
+
+        // In this case, self.current + 1 would point to some element within the sorted block,
+        // which we do not want to mess with. We can add one more to the sorted block.
+        if self.current >= self.first_sorted - 1 {
+            self.current = 0;
+            self.first_sorted -= 1;
+            // return false;
+        }
+
+        if data[self.current] > data[self.current + 1] {
+            // SAFETY: the indices self.current and self.current + 1 cannot possibly be the same,
+            // since no number is equal to itself plus 1.
+            let [a, b] =
+                unsafe { data.get_disjoint_unchecked_mut([self.current, self.current + 1]) };
+
+            std::mem::swap(a, b);
+        }
+
+        self.current += 1;
+        return false;
+    }
+}
